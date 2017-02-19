552d08d840a66e24eb3b36ec11138841250bff0a
leha <ALESHA-F51@yandex.ru>
2017-02-19 15:46:35 +0300

    Data классы

---
+++ WebApplication6_2/Models/DataClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6_2.Models
+{
+    public class DataImg
+    {
+        [Key]
+        public int id { get; set; }
+
+        public DateTime time { get; set; }
+
+        public string link { get; set; }
+
+        public string text { get; set; }
+
+        public byte[] data { get; set; }
+
+        public List<DataIP> requests { get; set; }
+    }
+
+    public class DataIP
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string ip { get; set; }
+
+        public DateTime time { get; set; }
+    }
+}
